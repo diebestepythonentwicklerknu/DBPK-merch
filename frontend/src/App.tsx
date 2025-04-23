a48f168bddb2d61eff88875feda6b20abd1a7a4b
anna <anna.androschuk@solidgate.com>
2025-04-23 19:06:53 +0300

    Fixed endpoints

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import "./App.scss";
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 
 function App() {
   return (
-      <div>
+      <div className="wrapper">
         <Header />
         <main>
         <Outlet />
--- frontend/src/api/apiProducts.ts
+++ frontend/src/api/apiProducts.ts
@@ -3,7 +3,13 @@ import { PRODUCTS } from "./api";
 
 export const getProducts = () => {
     return fetch(PRODUCTS)
-    .then(response => response.json());
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Unable to fetch products: ${response.statusText}`);
+        }
+
+        return response.json();
+    });
 };
 
 export const updateProductById = (productId: string, updatedProduct: UpdatedProduct) => {
@@ -12,9 +18,22 @@ export const updateProductById = (productId: string, updatedProduct: UpdatedProd
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(updatedProduct) 
     })
-    .then(response => response.json());
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Unable to update Product: ${response.statusText}`);
+        }
+
+        return response.json();
+    } );
 }
+
 export const deleteProductById = (productId: string) => {
     return fetch(`${PRODUCTS}${productId}`, { method: 'DELETE' })
-    .then(response => response.json());
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Unable to delete Product: ${response.statusText}`);
+        }
+
+        return response.json();
+    });
 }

a72d1d8c2099c321236aea8a2b5104648c9ca078
Fukuro <anya.androschuk@gmail.com>
2025-04-27 02:39:46 +0300

    fixed env link

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,4 @@
-const API_LINK = import.meta.env.API_LINK
+const API_LINK = import.meta.env.VITE_API_LINK
 
 export const PRODUCTS = `${API_LINK}/api/items/`
 export const ORDERS = `${API_LINK}/api/orders/`
